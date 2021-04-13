3a2e4d35ac79076e69fd92271b258b56186ec4f9
Salman1321 <67799390+Salman1321@users.noreply.github.com>
2021-04-13 23:23:28 +0600

    upload

---
+++ RAIL/header.php
@@ -0,0 +1,32 @@
+
+<head>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="headerstyle.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+<header>
+        <div class="menu-toggle" id="hamburger">
+            <i class="fas fa-bars"></i>
+        </div>
+        <div class="overlay"></div>
+        <div class="container">
+            <nav>
+                <h1 class="brand">W<span>E</span>L<span>C</span>O<span>M</span>E</h1>
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="availability.php">Availability</a></li>
+                    <li><a href="customer_reg.php">Sign Up</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="customer_log.php">Purchase Ticket</a></li>
+                    <li><a href="admin/">Admin</a></li>
+                </ul>
+            </nav>
+        </div>
+</header>
+
+
+</body>
