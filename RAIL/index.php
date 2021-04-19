752eb094123cf665031633d46e8d0b942141b02a
Salman_Rahman1722119042 <67799390+Salman1321@users.noreply.github.com>
2021-04-20 00:02:01 +0600

    Add files via upload

---
+++ RAIL/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,  maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="stylesheet" href="introstyle.css" type="text/css">
+  <link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+  
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+  
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+  
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+  <link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+  
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+  
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+  <title>Metro Rail Ticket Management</title>
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <h1><span>M</span>etro  <span>R</span>ail  <span>T</span>icket <span>M</span>anagement</h1>
+        </div>
+       
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>We Refuse <span></span></h1>
+        <h1>To Be Labeled <span></span></h1>
+        <h1>A Car Culture <span></span></h1>
+        <a href="home.php" type="button" class="cta">Enter</a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
+
+  
+  
+</body>
+</html>
