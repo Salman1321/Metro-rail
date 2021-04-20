79ae22eb0e71fda7bc45bb365ed68adc421424b8
Salman_Rahman1722119042 <67799390+Salman1321@users.noreply.github.com>
2021-04-21 00:12:57 +0600

    Add files via upload

---
+++ RAIL/services.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+  </head>
+  <body>
+  <?php include('header.php'); ?>
+    <div class="services-section">
+      <div class="inner-width">
+        <h1 class="section-title">Our Services</h1>
+        <div class="border"></div>
+        <div class="services-container">
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fas fa-subway"></i>
+            </div>
+            <div class="service-title">Stations</div>
+            <div class="service-desc">
+              You can see all the stations at ticket Purchase Page and confirm purchasing with desired stations.
+            </div>
+          </div>
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fas fa-ticket-alt"></i>
+            </div>
+            <div class="service-title">Purchase Ticket</div>
+            <div class="service-desc">
+              We provides one type of service for purchasing ticket. You can purchase ticket with custom selection as you want.
+            </div>
+          </div>
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fas fa-money-check-alt"></i>
+            </div>
+            <div class="service-title">PAYMENT</div>
+            <div class="service-desc">
+              Online payment is available with bkash/Credit card payment. You can also pay at any station counter.
+            </div>
+          </div>
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fab fa-searchengin"></i>
+            </div>
+            <div class="service-title">CHECK AVAILABILITY</div>
+            <div class="service-desc">
+              It's possible to check ticket availability before purchase even without login into the system.
+            </div>
+          </div>
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fa fa-train"></i>
+            </div>
+            <div class="service-title">Add Station</div>
+            <div class="service-desc">
+              Admin has  the privilege to add different stations as required.
+            </div>
+          </div>
+
+          <div class="service-box">
+            <div class="service-icon">
+              <i class="fas fa-clipboard-list"></i>
+            </div>
+            <div class="service-title">Limitation for Purchase</div>
+            <div class="service-desc">
+              Each person can purchase maximum 4 tickets as fraudulent person cannot harm to others.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
