3f6860ffcc4b597c728106f4081d6cfb38a6254f
1712397642_Ekbal <73793659+ShahjalalEkbal@users.noreply.github.com>
2021-04-26 01:24:14 +0600

    upload

---
+++ Admin/dashboard.php
@@ -0,0 +1,46 @@
+<?php include "inc/header.php"; ?>
+
+
+
+  <!-- Main Sidebar Container -->
+  <?php include "inc/menu.php"; ?>
+
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v1</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  <!-- Footer -->
+  <?php include "inc/footer.php"; ?>
+
+  <!-- Control Sidebar -->
+  <?php include "inc/sidebar.php"; ?>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<?php include "inc/script.php"; ?>
