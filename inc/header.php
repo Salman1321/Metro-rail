ec8730bd27cfe0123d4343e6b77caeed29b9d7a5
1712397642_Ekbal <73793659+ShahjalalEkbal@users.noreply.github.com>
2021-05-28 00:03:16 +0600

    upload

---
+++ inc/header.php
@@ -0,0 +1,26 @@
+<?php
+  ob_start();
+  include "lib/connection.php";
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Customer Records</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Data Table CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+    <!-- CSS File -->
+    <link rel="stylesheet" type="text/css" href="inc/style.css">
+  </head>
+
+  <body>
